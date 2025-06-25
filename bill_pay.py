a5c9429ef020ac828d1f25fe6a5edfc014e67f00
Nithin K P <nithinkp0212@gmail.com>
2025-06-25 22:06:47 +0530

    Update bill_pay.py

--- bill_pay.py
+++ bill_pay.py
@@ -1,4 +1,4 @@
 import random
-pay_bill= input("Enter Names...>>> ").split(" ")
+pay_bill= input("Enter Names...>>>  ").split(" ")
 bill_paid_by=random.choice(pay_bill)
 print(f"Bill Paid by {bill_paid_by}..Thank You..!")
